7233cdde69fe80e2a2d6896068de44f14dd0ed8f
André Souza <andresouzadev@gmail.com>
2020-06-04 02:46:58 -0300

    Nlw aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+
+import Routes from './routes'
+
 function App() {
+
+
   return (
 
-    <h1>Hello World</h1>
+    <div>
+      <Routes />
+    </div>
 
   );
 }
